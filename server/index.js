130c3db1babbe5d5bbc3f6661ebef0da7cfec5fe
jessikim <jessica.kim17@gmail.com>
2023-01-06 20:05:47 -0600

    add gitignore file

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
+  res.send({ hi: 'there :)' })
 })
 
-// const PORT = process.env.PORT
-app.listen(5050)
+const PORT = process.env.PORT || 5050
+app.listen(PORT)
