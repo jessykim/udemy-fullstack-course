773204dd258a1d22b25b618a3a4a07193eb91fc2
jessikim <jessica.kim17@gmail.com>
2023-01-07 15:10:07 -0600

    include passport and google strategy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import passport from 'passport'
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there :)' })
-})
+passport.use(new GoogleStrategy())
 
 const PORT = process.env.PORT || 5050
 app.listen(PORT)
