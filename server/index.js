f83002c9ff5c0d84a37e22cec7096192ca38569c
jessikim <jessica.kim17@gmail.com>
2023-01-07 16:51:42 -0600

    add get request for google callback uri

--- server/index.js
+++ server/index.js
@@ -1,10 +1,37 @@
 import express from 'express'
 import passport from 'passport'
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+import * as keys from './config/keys.js'
 
 const app = express()
 
-passport.use(new GoogleStrategy())
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken)
+      console.log('refresh token', refreshToken)
+      console.log('profile', profile)
+      console.log('done', done)
+    }
+  )
+)
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
+)
 
 const PORT = process.env.PORT || 5050
 app.listen(PORT)
