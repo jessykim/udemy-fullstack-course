e8308c965962bf55377b9a3d2112550910da4578
jessikim <jessica.kim17@gmail.com>
2023-01-08 20:16:47 -0600

    update google strategy settings and config files

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+// prod.js - production keys here!
+
+export const googleClientID = process.env.GOOGLE_CLIENT_ID
+export const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET
+export const mongoURI = process.env.MONGO_URI
+export const cookieKey = process.env.COOKIE_KEY
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -9,9 +9,17 @@ export default function authRoutes(app) {
   )
 
   app.get(
-    '/auth/google/callback',
-    passport.authenticate('google')
+    '/auth/google/callback', 
+    passport.authenticate('google'), 
+    (req, res) => {
+      res.redirect('/surveys')
+    }
   )
+
+  app.get('/api/logout', (req, res) => {
+    req.logout()
+    res.send(req.user)
+  })
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user)
