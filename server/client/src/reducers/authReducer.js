f8f33a0a79e06d4bc5eadc9b3a0278b9f9c7413c
jessikim <jessica.kim17@gmail.com>
2023-01-10 15:32:45 -0600

    update authReducer function

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,7 +1,10 @@
+import { FETCH_USER } from '../actions/types'
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default function(state = {}, action) {
-  console.log(action)
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state
   }
