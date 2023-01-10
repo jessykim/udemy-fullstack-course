f09b532d4d19ef58744b778637995604456ad6c3
jessikim <jessica.kim17@gmail.com>
2023-01-10 15:21:30 -0600

    change passport package version to fix error and refactor fetchUser function

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -6,10 +6,8 @@ class Header extends Component {
       <nav>
         <div className="nav-wrapper">
           <a href="/" className="left brand-logo">Emaily</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <ul className="right brand-logo">
             <li><a href="/auth/google">Login With Google</a></li>
-            {/* <li><a href="badges.html">Components</a></li>
-            <li><a href="collapsible.html">JavaScript</a></li> */}
           </ul>
         </div>
       </nav>
--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function(state = {}, action) {
+  console.log(action)
   switch (action.type) {
     default:
       return state
