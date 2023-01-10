0117e26ad50a802413b43a3bc09d23440de0e596
jessikim <jessica.kim17@gmail.com>
2023-01-10 14:52:43 -0600

    add hrefs to header, refactor app file and create actions file

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">Emaily</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="/auth/google">Login With Google</a></li>
+            {/* <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li> */}
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
