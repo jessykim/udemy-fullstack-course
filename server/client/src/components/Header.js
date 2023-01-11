97ae8fe021aa3b279b72c2b4710ed9a3e25fab7f
jessikim <jessica.kim17@gmail.com>
2023-01-11 14:49:48 -0600

    update routes and components for header, login and logout

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login With Google</a></li> 
+        );
+      default:
+        return <li><a href="/api/logout">Logout</a></li>
+    }
+  }
+
   render() {
+    console.log(this.props)
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="/" className="left brand-logo">Emaily</a>
-          <ul className="right brand-logo">
-            <li><a href="/auth/google">Login With Google</a></li>
+          <Link 
+            to={this.props.auth ? '/surveys' : '/' } 
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+          <ul className="right">
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +36,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
