8184f6dd5ddd650a7584b911d3cda0efc10458a3
jessikim <jessica.kim17@gmail.com>
2023-01-11 20:58:07 -0600

    create surveys route and middleware

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 require('./routes/authRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets like our main.js file, or main.css file
