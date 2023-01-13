4c9a4f9e0dc587af8259266da4a2339749fc71cb
jessikim <jessica.kim17@gmail.com>
2023-01-12 19:48:32 -0600

    add a redirect url to a thank you message

--- server/config/prod.js
+++ server/config/prod.js
@@ -5,5 +5,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY
+  sendGridKey: process.env.SEND_GRID_KEY,
+  redirectDomain: process.env.REDIRECT_DOMAIN
 }
