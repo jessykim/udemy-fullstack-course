3dc3146883896d4039f6023b02744d5663b9d372
jessikim <jessica.kim17@gmail.com>
2023-01-17 18:26:52 -0600

    create billing routes

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
