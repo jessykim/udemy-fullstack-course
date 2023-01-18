9e6264c5fc87ed852c5cc41e97f7821d0e5afe5b
jessikim <jessica.kim17@gmail.com>
2023-01-18 00:12:45 -0600

    refactor input fields on survey form

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,24 +1,34 @@
+import _ from 'lodash'
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import { Link } from 'react-router-dom'
 import SurveyField from './SurveyField'
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+]
 
 class SurveyForm extends Component {
   renderFields() {
-    return (
-      <div>
-        <Field label="Survey Title" type="text" name="title" component={SurveyField} />
-        <Field label="Subject Line" type="text" name="subject" component={SurveyField} />
-        <Field label="Email Body" type="text" name="body" component={SurveyField} />
-        <Field label="Recipient List" type="text" name="emails" component={SurveyField} />
-      </div>
-    )
+    return _.map(FIELDS, ({ label, name }) => {
+      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+    })
   }
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat red white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     )
