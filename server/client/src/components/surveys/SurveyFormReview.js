ef57bc72311d6e915511d2abb1a13e8ea616872b
jessikim <jessica.kim17@gmail.com>
2023-01-19 13:58:44 -0600

    finalize survey form review component displaying form values

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,28 +1,24 @@
+import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
+import formFields from './formFields'
 
 const SurveyReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>
+          {formValues[name]}
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <div>
-        <div>
-          <label>Survey Title</label>
-          <div>{formValues.title}</div>
-        </div>
-        <div>
-          <label>Subject line</label>
-          <div>{formValues.subject}</div>
-        </div>
-        <div>
-          <label>Email Body</label>
-          <div>{formValues.title}</div>
-        </div>
-        <div>
-          <label>Survey Title</label>
-          <div>{formValues.title}</div>
-        </div>
-      </div>
+      {reviewFields}
       <button
         className="yellow darken-3 btn-flat"
         onClick={onCancel}
